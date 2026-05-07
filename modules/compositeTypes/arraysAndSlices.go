a5f404b64c2ab86395f031637a17a0c5e1cbcf70
dipudangol2 <dipudangol2@gmail.com>
2026-05-07 19:51:39 +0545

    Arrays

---
+++ modules/compositeTypes/arraysAndSlices.go
@@ -0,0 +1,83 @@
+package main
+
+import "fmt"
+
+func ArrayDemo() {
+	fmt.Println("Array Demo:")
+
+	// 1D Array declaration and initialization. Must have the type specified during initialization
+	var arr [2]bool = [2]bool{true, false}
+	// Using composite literal
+	array := [2]int{1, 2}
+
+	fmt.Printf("Boolean Array: %v\n", arr)
+	fmt.Printf("Integer Array: %v\n", array)
+
+	// 2D Array declaration and initialization. [...][2] allows dynamic numbers of array for arrays of size 2
+	var twoDimensionalArr [2][2]int = [2][2]int{{1, 2}, {1, 3}}
+
+	// Using composite literal to declare. Has some flexibility for number of rows as they can be dynamic
+	twoDimensionalArr2 := [...][2]int{{1, 2}, {2, 3}, {3, 4}}
+
+	fmt.Printf("Two dimensional Array of size [2][2]: %v\n", twoDimensionalArr)
+	fmt.Printf("Two dimensional Array of size [%d][%d]: %v\n", len(twoDimensionalArr2), len(twoDimensionalArr2[0]), twoDimensionalArr2)
+
+	// Iterating on an array
+	//1D array: Iterating on arr
+	// Usual method with indexes
+
+	// 1D array
+	fmt.Print("Boolean Array iterated over with idx variable: [")
+	for idx := 0; idx < len(arr); idx++ {
+		fmt.Printf(" %v", arr[idx])
+	}
+	fmt.Print(" ]\n")
+
+	fmt.Printf("Two dimensional Array of size [2][2] iterated over with idx variable:[")
+	for idx := 0; idx < len(twoDimensionalArr); idx++ {
+		fmt.Print("[")
+		for idxCol := 0; idxCol < len(twoDimensionalArr[0]); idxCol++ {
+			fmt.Printf("%v", twoDimensionalArr[idx][idxCol])
+			if idxCol != len(twoDimensionalArr[0])-1 {
+				fmt.Print(",")
+			}
+		}
+		fmt.Print("]")
+		if idx != len(twoDimensionalArr)-1 {
+			fmt.Print(" ")
+		}
+
+	}
+	fmt.Print("]\n")
+
+	fmt.Print("Integer Array iterated over with range function implementation: [")
+	// If any value is not intended to be used then just use _ for the value.
+	//1D Array
+	for idx, elem := range array {
+		fmt.Printf("%v", elem)
+		if idx != len(array)-1 {
+			fmt.Print(" ")
+		}
+	}
+	fmt.Print("]\n")
+	// 2D Array
+	fmt.Printf("Two dimensional Array of size [%d][%d] iterated over range funciton: [", len(twoDimensionalArr2), len(twoDimensionalArr2[0]))
+	for idx, nested := range twoDimensionalArr2 {
+		fmt.Print("[")
+		for idx2, elem := range nested {
+			fmt.Printf("%v", elem)
+			if idx2 != len(nested)-1 {
+				fmt.Print(",")
+			}
+		}
+		fmt.Print("]")
+		if idx != len(twoDimensionalArr2)-1 {
+			fmt.Print(" ")
+		}
+	}
+	fmt.Print("]\n")
+}
+
+func SliceDemo(){
+	
+}
---
+++ modules/compositeTypes/main.go
@@ -0,0 +1,9 @@
+package main 
+
+func main(){
+	// Function for array demo
+	ArrayDemo()
+
+	// Function for slice demo
+	SliceDemo()
+}
