41e2613d6e1eadc876958f801ad5f85dcbe6e717
dipudangol2 <dipudangol2@gmail.com>
2026-05-14 21:57:51 +0545

    Functions as struct fields, embedded structs (structs inside structs)

--- modules/compositeTypes/structs.go
+++ modules/compositeTypes/structs.go
@@ -2,10 +2,23 @@ package main
 
 import "fmt"
 
+// Create a Job struct
+type Job struct {
+	name string
+	role string
+}
+
 // Create a person struct
 type Person struct {
 	name string
 	age  uint8
+	// You can also declare functions as fields inside a struct
+	introduce func(Person)
+	// Embedded structs (Structs inside struct)
+	jobs Job
+	job  []Job
+	// Can also have a slice like []Job
+
 }
 
 // Getters and Setters for the Person struct(Kind of like class methods in object oriented programming)
@@ -17,6 +30,10 @@ func (p Person) getName() string {
 
 func (p Person) getAge() uint8 {
 	return p.age
+}
+
+func (p Person) Introduction() {
+	fmt.Printf("Hello, My name is %s and I am %v years old.\n", p.name, p.age)
 }
 
 // Add pointers for if you want to modify the original content of the struct
@@ -32,7 +49,29 @@ func StructDemo() {
 	// Create an object for the struct type
 	p1 := Person{age: 23}
 	p1.setName("Dipu Dangol")
+	// Function needs to be initialized first to be used.
+	p1.introduce = func(p Person) {
+		fmt.Printf("Hello, My name is %s and I am %v years old.\n", p.name, p.age)
+	}
 	fmt.Println(p1.name, p1.age)
 	name := p1.getName()
 	fmt.Println(name)
+	// Function invocation
+	p1.introduce(p1)
+	// Reciever arguments look better in these types of cases. unless the function needs to be specific for each object type reciever arguments work for general methods.
+	p1.Introduction()
+
+	// Add the struct in the Person object
+	p1.jobs = Job{name: "Frontend Developer", role: "Junior"}
+	// Slice of Job struct
+	p1.job = make([]Job, 3)
+	fmt.Println(p1.jobs, p1.job)
+	jobNames := []string{"Backend Developer", "Software Engineer (DevOps)", "Project Manager"}
+	roles := []string{"Junior", "Mid", "Senior"}
+	// Populating like a normal slice
+	for idx := 0; idx < len(p1.job); idx++ {
+		p1.job[idx] = Job{name: jobNames[idx], role: roles[idx]}
+	}
+	fmt.Println(p1.job)
+
 }
