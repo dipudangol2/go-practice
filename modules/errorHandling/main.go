cca2c72777e39f8aa831072f57d6dd4a2e6008fb
dipudangol2 <dipudangol2@gmail.com>
2026-05-17 23:04:40 +0545

    "Using errors package in go to handle errors"

--- modules/errorHandling/main.go
+++ modules/errorHandling/main.go
@@ -1,13 +1,19 @@
 package main
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
 
 // Error handling in go is a bit different from other languages
 // Instead of try catch and finally there is panic, recover and defer
 
 // Create a divide function for demonstration of divide by zero
-func divide(a int, b int) int {
-	return a / b
+func divide(a int, b int) (int, error) {
+	if b == 0 {
+		return 0, errors.New("division by zero is not possible")
+	}
+	return a / b, nil
 }
 
 // Function which is deferred
@@ -24,16 +30,25 @@ func deferredFunc() {
 }
 
 func main() {
+	// Method 1: Using panic, defer and recover
+
 	// Defer usually executes after the main block finishes execution but if there is a panic defer runs first and panic is invoked
-	defer deferredFunc()
+	// defer deferredFunc()
 
 	// Create an error
 	// panic("This caused a crash") // any code after panic will not work
 
-	fmt.Println("Run") // This is unreachable if there is a panic above it
+	// fmt.Println("Run") // This is unreachable if there is a panic above it
 
 	// Invoke a panic from the divide function
-	divide(1, 0)
+	// divide(1, 0)
 
+	// Method 2: Use the errors package for error handling
+	result, err := divide(10, 0)
+	if err != nil {
+		fmt.Println("Error:", err)
+	} else {
+		fmt.Println("Result:", result)
+	}
+	// this is a better way to handle errors as it does not crash the program and allows us to handle the error gracefully
 }
-
