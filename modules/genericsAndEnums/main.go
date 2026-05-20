3c27a9a81443858b8d66dab9f50dfce102d6fdde
dipudangol2 <dipudangol2@gmail.com>
2026-05-20 21:43:23 +0545

    Implementation of enums in go using const and a method to get the string equivalent for the function

--- modules/genericsAndEnums/main.go
+++ modules/genericsAndEnums/main.go
@@ -3,9 +3,9 @@ package main
 import "fmt"
 
 // Any generic type can be passed in it
-func printSlice[T any](items []T) {
+func printSlice[T any, U string](items []T, name U) { // Multiple different generics can be passed to the function
 	for _, item := range items {
-		fmt.Println(item)
+		fmt.Println(item, name)
 	}
 }
 
@@ -29,22 +29,69 @@ type stack[T int | string] struct {
 	elements []T
 }
 
+// Enums in Go
+func changeOrderStatus(status orderStatus) {
+	fmt.Println("Order status changed to", status.parseString())
+}
+
+// There is no default enum type in go. We need to implement it in go with const or type.
+// we can use any other type like string or anything. I am using integers
+type orderStatus int
+
+const (
+	received orderStatus = iota
+	processing
+	prepared
+	shipped
+	delivered
+)
+
+// Create a utility function to map the int values to string
+func (s orderStatus) parseString() string {
+	switch s {
+	case received:
+		return "received"
+
+	case processing:
+		return "processing"
+
+	case prepared:
+		return "prepared"
+
+	case shipped:
+		return "shipped"
+
+	case delivered:
+		return "delivered"
+
+	default:
+		return "unknown"
+	}
+}
+
 func main() {
 	// fmt.Println("Generics and Enums in Go:")
-	items := []int{1, 2, 3, 4, 5}
-	printSlice(items)                      // This runs without any hiccup
-	printSlice([]string{"Dipu", "Dangol"}) // This also runs with seperate type passed
+	// items := []int{1, 2, 3, 4, 5}
+	// printSlice(items, "Dipu")                      // This runs without any hiccup
+	// printSlice([]string{"Dipu", "Dangol"}, "Dipu") // This also runs with seperate type passed
+	//
+	// // invoke the sum function with a slice of integers as well as float
+	// intSlice := []int{1, 2, 3, 4, 5, 6}
+	// floatSlice := []float64{1.35, 4.5533, 5.4, 6, 77.89}
+	//
+	// fmt.Println("Sum for the slice(", intSlice, "):", sum(intSlice...))
+	// fmt.Println("Sum for the slice (", floatSlice, ") with the same function using generic type:", sum(floatSlice...))
+	//
+	// // Create an object of stack for int and string. They need to be instantiated to be used.
+	// intStack := stack[int]{[]int{1, 2, 3, 4, 5}}
+	// stringStack := stack[string]{[]string{"Dipu", "Dangol"}}
+	// fmt.Println(intStack)
+	// fmt.Println(stringStack)
 
-	// invoke the sum function with a slice of integers as well as float
-	intSlice := []int{1, 2, 3, 4, 5, 6}
-	floatSlice := []float64{1.35, 4.5533, 5.4, 6, 77.89}
-
-	fmt.Println("Sum for the slice(", intSlice, "):", sum(intSlice...))
-	fmt.Println("Sum for the slice (", floatSlice, ") with the same function using generic type:", sum(floatSlice...))
-
-	// Create an object of stack for int and string. They need to be instantiated to be used.
-	intStack := stack[int]{[]int{1, 2, 3, 4, 5}}
-	stringStack := stack[string]{[]string{"Dipu", "Dangol"}}
-	fmt.Println(intStack)
-	fmt.Println(stringStack)
+	// Enums demo
+	changeOrderStatus(received)
+	changeOrderStatus(processing)
+	changeOrderStatus(prepared)
+	changeOrderStatus(shipped)
+	changeOrderStatus(delivered)
 }
