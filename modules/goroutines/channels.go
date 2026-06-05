57b7b46d97050a7be5e9591b2e83bf1e0e5b4b17
dipudangol2 <dipudangol2@gmail.com>
2026-06-05 22:18:45 +0545

    Practice channel with multiple workers and understand how parallel execution can be achieved with channels with a simple example

--- modules/goroutines/channels.go
+++ modules/goroutines/channels.go
@@ -2,12 +2,20 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
 	"sync"
+	"time"
 )
 
-func processNum(numChan chan int, wg *sync.WaitGroup) {
+func processNum(numChan chan int, wg *sync.WaitGroup, workerId int) {
 	defer wg.Done()
-	fmt.Println("Processing Number", <-numChan)
+
+	// The range closes once numChan is closed.
+	for num := range numChan {
+		fmt.Println("Processing Number", num, "Worker ID: ", workerId)
+		// Add a slight delay because if there is no delay there is a chance only one worker can process all the channels due to the worker only needing to take the value from the channel.
+		time.Sleep(time.Millisecond * 40)
+	}
 }
 
 func channelDemo() {
@@ -22,11 +30,26 @@ func channelDemo() {
 	// msg := <-messageChannel
 	//
 	// fmt.Println(msg)
+
+	// Initialize a waitgroup and channel for integer
 	var wg sync.WaitGroup
 	numChan := make(chan int)
 
-	wg.Add(1)
-	go processNum(numChan, &wg)
-	numChan <- 5
+	// We want to have multiple workers for the processing of data in the channel
+	// We Initialize the number of workers and start the execution for all the workers
+	workerNum := 5
+	for i := 1; i <= workerNum; i++ {
+		wg.Add(1)
+		go processNum(numChan, &wg, i)
+	}
+
+	// send 30 items to the pipeline
+	for range 30 {
+		numChan <- rand.Intn(100)
+	}
+
+	// It is important to close the channel once the data has been sent completely to avoid deadlock while waiting for the numbers in the channel.
+	close(numChan)
+
 	wg.Wait()
 }
