4da6c0fce0ea30a7ddc7095b4ed51352d7efe2c5
dipudangol2 <dipudangol2@gmail.com>
2026-06-11 21:31:43 +0545

    Using mutex in go. How to lock and unlock and avoid bottlenecks.

--- modules/mutex/main.go
+++ modules/mutex/main.go
@@ -10,7 +10,11 @@ import (
 * */
 
 // Example of mutex using a struct post
+// Mutex is a synchronization technique which locks a specific resource until the operation on it is completed and then releases it. It follows a strict ownership where the thread which locks the resource is only allowed to unlock the resource for others to access
+// While mutex does help in avoiding race condition it can also cause bottlenecks as the resource is held up by the goroutine which accesses it first. We can use channels for better performance on larger number of goroutines and complex operations. Mutex are preferred on atomic and smaller operations
 type post struct {
+	// Create a mutex. After a mutex is declared inside a struct the struct object should always be passed by reference not value or copy.
+	mut   sync.Mutex
 	views int
 }
 
@@ -22,6 +26,12 @@ func createPost() post {
 
 // function which increments the post view count by 1
 func (p *post) viewPost() {
+	// Unlock the resource after the operation on it is complete. If not done can cause DEADLOCKS
+	// defer so that the mutex is unlocked even if the function crashes
+	defer p.mut.Unlock()
+	// only keep the modification inside the lock so that it doesnot become a burden on speed. for eg if there are other operations keep it above these lines
+	// Lock the resource before operating on it
+	p.mut.Lock()
 	p.views += 1
 }
 
@@ -45,6 +55,7 @@ func main() {
 
 		// Use multiple workers to view the post concurrently
 		// this causes race condition, the count is not always correct as the workers can interrupt this
+		// After mutex implementation each run should have the full count of the views for the post.
 		workers := 5
 		for i := 0; i < workers; i++ {
 			wg.Go(
